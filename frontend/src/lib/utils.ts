81ddf24e78951fe983e560dde7eaf88bce91e6d8
yennie <l.yennie.j@gmail.com>
2025-07-07 23:29:43 +0900

    타입 수정

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -31,7 +31,7 @@ export function calculateProgress(completed: number, total: number): number {
   return Math.round((completed / total) * 100);
 }
 
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => unknown>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
